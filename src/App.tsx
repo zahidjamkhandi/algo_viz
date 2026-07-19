75006f15aceebfabc062efd64c0a5ff64d0a9fd2
zahidjamkhandi <41178699+zahidjamkhandi@users.noreply.github.com>
2026-07-19 20:49:23 +0530

    fixed: scroll up on routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { SortingVisualizer } from './pages/SortingVisualizer';
+import ScrollToTop from './components/utility/scrollToTop';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/sort" element={<SortingVisualizer />} />
